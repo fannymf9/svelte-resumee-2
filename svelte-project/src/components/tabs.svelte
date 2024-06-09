2ec4041669c1a2e8e29f8e8dda706b4c310e3802
fannymf9 <fanny.ferreira@essec.edu>
2024-06-09 16:55:47 +0200

    Added interactive features and updated README

---
+++ svelte-project/src/components/tabs.svelte
@@ -0,0 +1,34 @@
+<script>
+    export let tabs = [];
+    export let selectedTab = 0;
+  
+    function selectTab(index) {
+      selectedTab = index;
+    }
+  </script>
+  
+  <div class="tabs">
+    {#each tabs as tab, index}
+      <button on:click={() => selectTab(index)} class:active={selectedTab === index}>
+        {tab.label}
+      </button>
+    {/each}
+  </div>
+  
+  <div class="tab-content">
+    {#if tabs[selectedTab]}
+      <slot name={tabs[selectedTab].name} />
+    {/if}
+  </div>
+  
+  <style>
+    .tabs {
+      display: flex;
+      gap: 1rem;
+    }
+    .tabs button.active {
+      font-weight: bold;
+    }
+  </style>
+
+  
